688cd1e77d39ea53b08d1981ff78482d6bbe2d12
Bozhidar Slaveykov <bobi@microweber.com>
2022-08-09 13:43:09 +0300

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
         <div class="login-holder">
             <h2>{{ trans('all.login_title') }} </h2>
 
-            <div class="my-4"><a href="{{ url('/register') }}" class="">{{ trans('all.sign_up_now') }}</a></div>
+            <div class="my-4"><a href="{{ url('/register') }}" class="btn btn-outline-primary">{{ trans('all.sign_up_now') }}</a></div>
 
             <p>{!! trans('all.login_desc') !!}</p>
             <br>
