017b484906c25923286deb7529dece294cd32177
Bozhidar Slaveykov <bobi@microweber.com>
2022-11-22 14:49:03 +0200

    update

--- app/Models/User.php
+++ app/Models/User.php
@@ -79,6 +79,6 @@ class User extends Authenticatable implements HasLocalePreference
 
     public function preferredLocale()
     {
-        return $this->locale; 
+        return $this->locale;
     }
 }
